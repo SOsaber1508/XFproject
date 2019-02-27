5ad87f01ddba3a4495ee52c6bb13f7faf4e3df31
SOsaber1508 <32865478+SOsaber1508@users.noreply.github.com>
2019-02-27 09:25:18 +0800

    接口

--- src/main/java/com/fec/epreport/util/IdcardUtil.java
+++ src/main/java/com/fec/epreport/util/IdcardUtil.java
@@ -1,5 +1,44 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
 package com.fec.epreport.util;
 
+
+import java.net.URLEncoder;
+
+/**
+ * 身份证识别
+ */
 public class IdcardUtil {
 
+    /**
+     * 重要提示代码中所需工具类
+     * FileUtil,Base64Util,HttpUtil,GsonUtils请从
+     * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
+     * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
+     * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
+     * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3
+     * 下载
+     */
+    public static void main(String[] args) {
+        // 身份证识别url
+        String idcardIdentificate = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard";
+        // 本地图片路径
+        String filePath = "#####本地文件路径#####";
+        try {
+            byte[] imgData = FileUtil.readFileByBytes(filePath);
+            String imgStr = Base64Util.encode(imgData);
+            // 识别身份证正面id_card_side=front;识别身份证背面id_card_side=back;
+            String params = "id_card_side=front&" + URLEncoder.encode("image", "UTF-8") + "="
+                    + URLEncoder.encode(imgStr, "UTF-8");
+            /**
+             * 线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
+             */
+            String accessToken = "#####调用鉴权接口获取的token#####";
+            String result = BaiduHttpUtil.post(idcardIdentificate, accessToken, params);
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
