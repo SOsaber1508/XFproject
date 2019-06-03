4db45ae1a65f554934fbe66f08e52793ba568c15
SOsaber1508 <32865478+SOsaber1508@users.noreply.github.com>
2019-06-03 10:01:12 +0800

    入驻+接口

---
+++ src/main/java/com/fec/epreport/controller/cooperationIn/StationConttroller.java
@@ -0,0 +1,33 @@
+package com.fec.epreport.controller.cooperationIn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fec.epreport.util.commons.PureNetUtil;
+
+
+/**
+ * 接收json用测试接口
+ * 
+ * @author zcy
+ *
+ */
+@Controller
+@RequestMapping("/station")  
+public class StationConttroller {
+
+	
+	
+}
